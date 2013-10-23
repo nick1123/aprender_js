bff7aa7e1281361294c8fc04f2218389028f1de6
Nick Stewart <nicholas4@gmail.com>
2013-10-23 02:48:10 +0000

    Move game data to its own file

---
+++ us_state_capitals_small.js
@@ -0,0 +1,13 @@
+    var questionsAndAnswers = {
+        "Alabama": "Montgomery",
+        "Alaska": "Juneau",
+        "Arizona": "Phoenix",
+        "Arkansas": "Little Rock"};
+
+    var questions = [
+        "Alabama",
+        "Alaska",
+        "Arizona",
+        "Arkansas"];
+
+
